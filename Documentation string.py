7bb4c4be44a6c86b854fe637e6c9c1a18e9650a2
sakshiasutar <105741089+sakshiasutar@users.noreply.github.com>
2022-06-22 21:10:47 +0530

    Create Documentation string.py
    How to access Documentation string

---
+++ Documentation string.py
@@ -0,0 +1,9 @@
+def sum(a,b):
+    '''This is sum function
+    to perform Addition'''
+    c=a+b
+    print(c)
+print(sum.__doc__)
+n1=int(input("enter any value"))
+n2=int(input("enter any vaule"))
+sum(n1,n2)
