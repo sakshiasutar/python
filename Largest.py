f0ff1c8a8c88be3857865d57601f795e395ee6b0
sakshiasutar <105741089+sakshiasutar@users.noreply.github.com>
2022-06-23 20:44:53 +0530

    Create Largest.py
    Using def function print largest no. from 3 nos

---
+++ Largest.py
@@ -0,0 +1,15 @@
+def max(a,b,c):
+    if a>b:
+        if a>b:
+            print(a)
+        else:
+            print(c)
+    else:
+         if b>c:
+             print(b)
+         else:
+             print(c)
+n1=int(input("Enter any value"))
+n2=int(input("Enter any value"))
+n3=int(input("Enter any value"))
+max(n1,n2,n3)
